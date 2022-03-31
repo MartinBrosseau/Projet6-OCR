b115c525b8bd1f83d184283cb92b187c4ad4add6
Martin Brosseau <martinbrosseau@hotmail.fr>
2022-03-31 17:05:28 +0200

    login + password function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
 
 mongoose.connect('mongodb+srv://MartinBrosseau:databasepassword@cluster0.giuct.mongodb.net/Cluster0?retryWrites=true&w=majority',
     { useNewUrlParser: true,
@@ -21,5 +23,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
